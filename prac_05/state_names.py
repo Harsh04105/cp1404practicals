7cadabf0fac6b555ee354ae67a2a6623195dbfdc
Harsh04105 <harshpreet.bhullar@my.jcu.edu.au>
2025-10-15 10:55:13 +1000

    Apply string formatting and use exceptions

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,18 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+max_length = max(len(name) for name in list(CODE_TO_NAME.values()))
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name:{max_length}}")
+
+
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
