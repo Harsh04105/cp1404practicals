bcf5fe4ae31782c0d84a2a5d310d07cc1bb4bca1
Harsh04105 <harshpreet.bhullar@my.jcu.edu.au>
2025-11-19 13:08:33 +1000

    Complete Prac 9 Band

---
+++ prac_09/band.py
@@ -0,0 +1,15 @@
+from musician import Musician
+
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        return f"{self.name} ({','.join(str(musician) for musician in self.musicians)})"
+
+    def add(self, musician: Musician):
+        self.musicians.append(musician)
+
+    def play(self):
+        return '\n'.join(musician.play() for musician in self.musicians)
